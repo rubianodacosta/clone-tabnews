d3cfcf0c1ace54bea6f56456d9cf4b21190863a6
Rubiano da Costa <81049319+rubianodacosta@users.noreply.github.com>
2025-07-18 18:24:21 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Tenha fé e paciência , logo tudo vai dar certo, se você me ama, dá uma risadinha</h1>
+    return <h1>Tenha fé e paciência , logo tudo vai dar certo, se você me ama, dá uma risadinha !</h1>
 }
 
 export default Home;
