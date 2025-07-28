da765181246261d48a6b1477b1914d3aeeb7ec90
Rubiano da Costa <81049319+rubianodacosta@users.noreply.github.com>
2025-07-28 18:59:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do 0
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Tenha fé e paciência , logo tudo vai dar certo, se você me ama, dá uma risadinha !</h1>
+function Home() {
+  return (
+    <h1>
+      Tenha fé e paciência , logo tudo vai dar certo, se você me ama, dá uma
+      risadinha !
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
